e7afcdbda34acdfe6b933ebed6493365d2cdb46b
Connor Bush <connormbush12@gmail.com>
2021-02-01 22:14:53 -0600

    Destructured state, improved ternary expressions

--- src/screens/ColorTintScreen.js
+++ src/screens/ColorTintScreen.js
@@ -6,17 +6,20 @@ const COLOR_INCREMENT = 15;
 const reducer = (state, action) => {
     switch(action.type) {
         case 'red':
-            return state.red + action.payload > 255 || state.red + action.payload < 0 ? {...state} : {...state, red: state.red + action.payload}
+            return state.red + action.payload > 255 || state.red + action.payload < 0 ? state : {...state, red: state.red + action.payload}
         case 'green':
-            return state.green + action.payload > 255 || state.green + action.payload < 0 ? {...state} : {...state, green: state.green + action.payload}
+            return state.green + action.payload > 255 || state.green + action.payload < 0 ? state : {...state, green: state.green + action.payload}
         case 'blue':
-            return state.blue + action.payload > 255 || state.blue + action.payload < 0 ? {...state} : {...state, blue: state.blue + action.payload}
+            return state.blue + action.payload > 255 || state.blue + action.payload < 0 ? state : {...state, blue: state.blue + action.payload}
+        default:
+            return state;
         }
-
 }
+
 const ColorTintScreen = () => {
     const [state, dispatch] = useReducer(reducer, {red: 0, green: 0, blue: 0})
-    
+    const {red, green, blue} = state
+
     return (
         <View>
             <ColorButton 
@@ -34,7 +37,7 @@ const ColorTintScreen = () => {
                 onIncrease={()=> {dispatch({type: 'blue', payload: COLOR_INCREMENT})}}
                 onDecrease={()=> {dispatch({type: 'blue', payload: -1 * COLOR_INCREMENT})}}
             />
-            <View style={{width:150, height:150, backgroundColor: `rgb(${state.red}, ${state.green}, ${state.blue})`}}></View>
+            <View style={{width:150, height:150, backgroundColor: `rgb(${red}, ${green}, ${blue})`}}></View>
         </View>
     )
     }
